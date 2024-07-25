69e9460f1e7b45ce2debaf9c335e417bf1ce8f44
Derek Worthen <worthend.derek@gmail.com>
2024-07-25 08:08:39 -0700

    adding

--- packages/archive-downloader/src/utils/index.ts
+++ packages/archive-downloader/src/utils/index.ts
@@ -1,4 +1,4 @@
-import { execFile } from 'node:child_process'
+import { execFile, ExecFileOptions } from 'node:child_process'
 import { promisify } from 'node:util'
 
 const execFileAsync = promisify(execFile)
@@ -10,11 +10,15 @@ export type BinReturn = {
 
 export function wrapBin(
   binPath: string,
+  options?: ExecFileOptions,
 ): (...args: string[]) => Promise<BinReturn> {
   const ext = process.platform === 'win32' ? '.exe' : ''
   binPath += ext
 
   return async function run(...args: string[]): Promise<BinReturn> {
-    return await execFileAsync(binPath, args)
+    return await execFileAsync(binPath, args, {
+      ...options,
+      encoding: 'utf-8',
+    })
   }
 }
